9829453b75861e1ec014b96126754850bd69662f
alison <71641175+damacusdos@users.noreply.github.com>
2024-03-17 20:26:35 +0800

    add basename to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-import svgr from "vite-plugin-svgr";
+import { vitePlugin as remix } from '@remix-run/dev';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/remix-spa/' : '/',
   plugins: [
     remix({
+      basename: process.env.NODE_ENV === 'production' ? '/remix-spa/' : '/',
       ssr: false,
     }),
     tsconfigPaths(),
